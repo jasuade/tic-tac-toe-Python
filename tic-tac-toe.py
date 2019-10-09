342887a053c5f4ecc174d85b934011b8fa4fad3c
jasuade <jasuade@dcom.upv.es>
2019-10-09 19:28:31 +0200

    Added function to check the board for 3-line

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -13,11 +13,9 @@ class game:
     for i in self.board[0]:
       print '|',i,
     print '| \n'
-    print '------------- \n'
     for i in self.board[1]:
       print '|',i,
     print '| \n'
-    print '------------- \n'
     for i in self.board[2]:
       print '|',i,
     print '|\n------------- \n'
@@ -32,16 +30,43 @@ class game:
       self.board[x][y] = self.players[player]
     self.drawBoard()
 
+#Check if there is a line, there is a winner
+  def checkMatrix(self):
+    for idx, elem in enumerate(self.board):
+      self.checkLine(elem)
+    #Definitely this needs yo be improved
+    self.checkLine([self.board[0][0], self.board[1][0], self.board[2][0]])
+    self.checkLine([self.board[0][1], self.board[1][1], self.board[2][1]])
+    self.checkLine([self.board[0][2], self.board[1][2], self.board[2][2]])
+    self.checkLine([self.board[0][0], self.board[1][1], self.board[2][2]])
+    self.checkLine([self.board[0][2], self.board[1][1], self.board[2][0]])
+
+  def checkLine(self, line):
+    if line.count(1) == 3:
+      print 'Player 1 wins the game'
+    elif line.count(-1) == 3:
+      print 'Player 2 wins the game'
+
+
+
 #----------------TEST--------------------------
 g1 = game()
 #player = random.choice ([0,1])
 player = 1
+#exit = 0
 
 while (1):
+  #if player == 1:
   print "Set position x,y axes",
   move = input()
   if 0 <= move[0] and move[1] < 3:
     g1.drawBoard()
     g1.setMove(move, player)
+    g1.checkMatrix()
   else:
     print 'Movement out of board'
+  #if playyer == -1 # the machine
+      #checkMatrix():
+      #move = decideMove():
+      #setMove(move, player)
+      #player = 1
