1bcc13f29b01e362ec6263bacd547bf957d696d5
jasuade <jasuade@dcom.upv.es>
2019-10-11 14:02:37 +0200

    Taken totally different approach just to test the behaviour, still implementing the machineMove

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -8,6 +8,8 @@ class game:
     self.exit = 1
     self.turn = 0
     self.players = ['X', 'O'] # 'X' is the User and 'O' the computer
+    self.positionsX = []
+    self.positionsO = []
 
 #Very ugly improved way to draw a board in the cmd (v2)
   def drawBoard(self):
@@ -53,12 +55,12 @@ class game:
     return
 
   def sumLine(self, line):
-    return line.count('X')
+    return line.count('O')
+
 
   '''
   Funtion for the computer to decide in wich position locate the symbol 'O'
   TODO: to be impoved
-  '''
   def machineMove(self):
     # If there is nothing in the central position --> fill the center
     if self.board[1][1] == 0:
@@ -103,12 +105,79 @@ class game:
       move = [x,y]      
       return move
     # TODO: check if the random is already taken, chose another
+    # TODO: If the board is completed --> stalemate/tie
+    '''
+
+  def machineMove(self):
+    for i in range(len(self.board)):
+        for j in range(len(self.board[i])):
+          if self.board[i][j] == 'X':
+            self.positionsX.append([i,j])
+          elif self.board[i][j] == 'O':
+            self.positionsO.append([i,j])
+    if len(self.positionsO) == 0:
+      x = random.choice([0,1,2])
+      y = random.choice([0,1,2])
+      move = [x,y]
+      self.positionsX.append([x,y])
+      return move
+    elif (len(self.positionsO) == 1) and (len(self.positionsX) == 0):
+      #'O' in center position
+      if self.positionsO[1] == [1,1]:
+        x = random.choice([0,2])
+        y = random.choice([0,2])
+        self.positionsX.append([x,y])
+        return [x,y]
+      else:
+        self.positionsX.append([1,1])
+        return [x,y]
+    #elif (len(self.positionsO) == 1) and (len(self.positionsX) == 1):
+    #elif (len(self.positionsO) == 2) and (len(self.positionsX) == 1):
+    #If there is any row with two 'O' --> complete the line || If there is a row with two 'X' --> break the line
+    else:
+      for i in range(len(self.board)):
+        if self.board[i].count('O') == 2:
+          for j in range(len(self.board[i])):
+            if self.board[i][j] == 0:
+              move = [i,j]
+              return move
+        if self.board[i].count('X') == 2:
+          for j in range(len(self.board[i])):
+            if self.board[i][j] == 0:
+              move = [i,j]
+              return move
+        #Check columns 
+        if self.sumLine([self.board[0][i], self.board[1][i], self.board[2][i]]) == 2:
+          print self.sumLine([self.board[i][0], self.board[i][0], self.board[i][0]])
+          for j in range(len(self.board[i])):
+            if self.board[j][i] == 0:
+              move = [j,i]
+              return move
+        #Check diagonals
+        if self.sumLine([self.board[0][0], self.board[1][1], self.board[2][2]]) == 2:
+          for j in range(len(self.board[i])):
+            if self.board[j][j] == 0:
+              move = [j,j]
+              return move
+        elif self.sumLine([self.board[0][-1], self.board[1][-2], self.board[2][-3]]) == 2:
+          for j in range(-1,-3):
+            if self.board[i][j] == 0:
+              move = [i,j]
+              return move
+    return [1,2]
+
+    
+
+
+
+
 
 
 #----------------TEST--------------------------
 g1 = game()
 g1.drawBoard()
-player = random.choice ([0,1])
+#player = random.choice ([0,1])
+player = 0
 
 
 while (g1.exit):
@@ -119,11 +188,14 @@ while (g1.exit):
       g1.setMove(move, player)
       g1.checkMatrix()
       sleep(2)
+      g1.turn += 1
       player = 1
     else:
       print 'Movement out of board'
   if player == 1:                              # The Computer
     move = g1.machineMove()
+    print move
     g1.setMove(move, player)
     g1.checkMatrix()
+    g1.turn += 1
     player = 0
